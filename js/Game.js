f4c47265557a355394df1fdbf98b401f0546cf0c
Josh Seltzer <jselt88@gmail.com>
2020-11-03 03:43:51 -0500

    1

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,9 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor() {
+
+    }
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,36 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+
+    /**
+     * Adds letter placeholders to the display on game start
+     * TODO: one li element for each letter
+     * TODO: Correct css usage for letter vs. space placeholders
+     * TODO: include class or id attributes needed
+     */
+    addPhraseToDisplay() {
+
+    }
+
+    /**
+    * Checks to see if the letter selected by player matches a letter in the phrase
+    *
+    */
+    checkLetter() {
+
+    }
+
+    /**
+     * Reveals the letter(s) on the board when selected by playe
+     * TODO: To reveal the amtching letter(s), select all letter DOM elements that have CSS class name that matches the selected letter
+     * TODO: and replace element's css hide class with css show class
+     */
+    showMatchedLetter() {
+
+    }
+}
